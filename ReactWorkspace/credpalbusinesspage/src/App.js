6e53f721b2fa1091900ae48e6f54f4de97192b05
Oluwadamilola Adediran <dammyadediran94@gmail.com>
2023-03-21 14:50:01 +0100

    done

--- ReactWorkspace/credpalbusinesspage/src/App.js
+++ ReactWorkspace/credpalbusinesspage/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './components/header';
 import Footer from './components/footer';
+import Home from './components/home';
 
 function App() {
   return (
     <div className="App">
      <Header/>
+     <Home />
      <Footer />
     </div>
   );
--- ReactWorkspace/credpalbusinesspage/src/components/home.js
+++ ReactWorkspace/credpalbusinesspage/src/components/home.js
@@ -1,14 +1,9 @@
 import React from "react";
-import womansmile from "../assests/images/womansmile.svg";
 import "../assests/css/home.css";
 import CustomButton from "../library/custombutton";
-import Payment from "./paymentcard";
-import Faq from "./faq";
-
-
-
-
-
+// import Payment from "./paymentcard";
+import background from "../assests/images/backgroundimage.svg";
+// import Faq from "./faq";
 
 const Home = () => {
   return (
@@ -16,37 +11,44 @@ const Home = () => {
       <div className="financialneeds_container">
         <div className="financial_needs">
           <div>
-            <p>Join several fast-growing businesses and watch your sales increase by over 32%</p>
-          <img src={imagebackground} alt="image" />
-
+            <p>
+              Join several fast-growing businesses and watch your sales increase
+              by over 32%
+            </p>
+            <img src={background} alt="background"/>
           </div>
-          
-        </div>
-        <div className="phone_container">
-          <img src={womansmile} alt="iphone" />
         </div>
       </div>
       <div className="customers_container">
         <div className="our_customers">
-          <h2>Save more by using four strategies.</h2>
-          <p>Maximize your savings and reach your financial goals faster with CredPal! With CredPal, you have the opportunity to receive up to 18% returns on your savings, making your money grow faster and helping you achieve your financial goals sooner.</p>
+          <h4>THE CREDPAL ADVANTAGE</h4>
+          <p>
+            Increase conversion for your business
+          </p>
+          <p>Expand your payment options to include CredPal Pay. This way, your customers no longer abandon cart. 
+            You get full payment at the point of sale, while customers pay us later. It's a win-win for all.</p>
         </div>
         <div>
-          <Payment/>
-      </div>
+          {/* <Payment /> */}
+        </div>
       </div>
       <div>
-         <div>
-          <h1>FAQs</h1>
-         </div>
-         <div>
-          <Faq />
-         </div>
+        <div>
+          {/* <h1>FAQs</h1> */}
+        </div>
+        <div>
+          {/* <Faq /> */}
+        </div>
       </div>
       <div className="building_container">
         <h2>Start building your credit score today</h2>
-        <p>Download our app to get started on your credit journey in Africa. Make the first move towards a better financial future today.</p>
-        <CustomButton textColor="#3772FF" buttonWidth="12rem">Sign Up</CustomButton>
+        <p>
+          Download our app to get started on your credit journey in Africa. Make
+          the first move towards a better financial future today.
+        </p>
+        <CustomButton textColor="#3772FF" buttonWidth="12rem">
+          Sign Up
+        </CustomButton>
       </div>
     </div>
   );
