59ff53fee142cc89e8f60733a6da53cc9dc65577
dammysaina <dammyadediran94@gmail.com>
2024-05-18 00:00:52 +0100

    sidebar

---
+++ ReactWorkspace/sonarwatch/src/components/sidebar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Logo from "../assests/images/logo.svg"
+
+const sidebar = () => {
+  return (
+    <div>
+      <img src={Logo} alt='sonarwtchlogo' />
+    </div>
+  )
+}
+
+export default sidebar
