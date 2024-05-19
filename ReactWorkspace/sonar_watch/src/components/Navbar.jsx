4ca34d44313a844cb031db6c2f35c8efbaf7e9bd
dammysaina <dammyadediran94@gmail.com>
2024-05-19 01:24:18 +0100

    images

--- ReactWorkspace/sonar_watch/src/components/Navbar.jsx
+++ ReactWorkspace/sonar_watch/src/components/Navbar.jsx
@@ -6,11 +6,11 @@ import SearchIcon from "../assets/images/searchicon.svg"
 const Navbar = ({ toggleDarkMode }) => {
   return (
     <div className="fixed top-0 left-0 w-full flex justify-between items-center p-4 bg-white border-b-2 border-customGray z-50 ">
-      <div >
+      <div className="border-r-2 border-customGray">
         <img src={SonarLogo} alt="sonarlogo" style={{width: "180px"}}/>
       </div>
-
-      <div class="flex justify-between items-center h-14 header-right border-l-4 border-customGray">
+        
+      <div class="flex justify-between items-center h-14 header-right">
         <div class="bg-searchIcon rounded-md flex items-center w-full max-w-xl mr-4 p-2 shadow-sm focus:ring-2 focus:ring-green-700 focus:outline-none">
           <input
             type="search"
@@ -23,6 +23,7 @@ const Navbar = ({ toggleDarkMode }) => {
         </div>
         <img src={BlackAddress} style={{ width: "20px" }}alt="blackaddress"/>
       </div>
+
     </div>
   );
 };
